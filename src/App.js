2d51babea3aa9627453a6212bfddccb7f203f6c7
Faxriyor <faxriyor@gmail.com>
2021-06-30 17:14:07 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+export default class App extends Component {
+    notify = () => toast.success('Salom hammaga')
+    render() {
+        return (
+            <div>
+                        <button onClick={this.notify}>Notify!</button>
+                        <ToastContainer
+                            pauseOnHover
+                            position="bottom-left"
+                            draggable
+                            
+                        />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            </div>
+        )
+    }
 }
-
-export default App;
